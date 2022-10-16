4969bf66ab3740465b76bead7f0eb4053fbe02e8
ananyabansalcode <115916378+ananyabansalcode@users.noreply.github.com>
2022-10-16 12:25:27 +0530

    Update pallindrome.cpp

--- pallindrome.cpp
+++ pallindrome.cpp
@@ -1,20 +1,35 @@
-#include <iostream>  
-using namespace std;  
-int main()  
-{  
-  int n,r,sum=0,temp;    
-  cout<<"Enter the Number=";    
-  cin>>n;    
- temp=n;    
- while(n>0)    
-{    
- r=n%10;    
- sum=(sum*10)+r;    
- n=n/10;    
-}    
-if(temp==sum)    
-cout<<"Number is Palindrome.";    
-else    
-cout<<"Number is not Palindrome.";   
-  return 0;  
-}  
+// C++ program for the above approach
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to check whether
+// the string is palindrome
+string isPalindrome(string S)
+{
+	// Stores the reverse of the
+	// string S
+	string P = S;
+
+	// Reverse the string P
+	reverse(P.begin(), P.end());
+
+	// If S is equal to P
+	if (S == P) {
+		// Return "Yes"
+		return "Yes";
+	}
+	// Otherwise
+	else {
+		// return "No"
+		return "No";
+	}
+}
+
+// Driver Code
+int main()
+{
+	string S = "ABCDCBA";
+	cout << isPalindrome(S);
+
+	return 0;
+}
